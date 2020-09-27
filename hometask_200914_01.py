bc3e8005d4ec05b7fdc2db645860d9126b42770f
vldln-biven'мамы.net <strifee322@gmail.com>
2020-09-27 19:18:06 +0500

    dates=mark

--- hometask_200914_01.py
+++ hometask_200914_01.py
@@ -4,22 +4,21 @@ while True:
     if mark:
         student_marks.append(int(mark))
         if int(mark) > 5:
-            print('неверная оценка (>5)')
+            print('неверно введена оценка')
             student_marks.remove(int(mark))
         elif int(mark) <= 1:
-            print('неверная оценка (<=1)')
+            print('неверно введена оценка')
             student_marks.remove(int(mark))
     else:
         break
-
-print('ввод завершен')
+        print('ввод завершен')
 
 avg_mark = 0
 for marks in student_marks:
     avg_mark += marks
-    avg_mark /= len(student_marks)
-    if not student_marks:
-        print('вы не ввели оценку')
-    else:
-        print('Marks:', student_marks)
-        print('средняя оценка:', avg_mark)
+avg_mark /= len(student_marks)
+if not student_marks:
+    print('')
+else:
+    print('marks:', student_marks)
+    print('Средний балл:', avg_mark)
