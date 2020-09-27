7f09b952ac5724ea6838c79d1116b160b0690487
vldln-biven'мамы.net <strifee322@gmail.com>
2020-09-27 19:21:29 +0500

    strings

---
+++ hometask_200914_03.py
@@ -0,0 +1,22 @@
+a = 'QwErTy'.lower()
+print(a)
+
+
+z = 'TEPLOV VLADLEN'.upper()
+print(z)
+
+
+
+s='PYTHON'
+print(s.lower())
+
+
+
+s='VLADLEN NESTEROV'
+print(s.capitalize())
+
+
+
+
+x='TEPLOV VLADLEN ONIDZUKOVICH'
+print(len(x))
